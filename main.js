ada443ec64c1f9f8a69850d9e6f1cf8e33c82776
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-02-03 17:19:38 +0700

    Create function to change grid item to single color when mouse enter

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const div = document.createElement("div");
-    div.classList.add("grid");
-    container.appendChild(div);
+createGrid();
+
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const div = document.createElement("div");
+      div.classList.add("grid");
+      container.appendChild(div);
+    }
   }
+}
+
+const grid = document.querySelectorAll(".grid");
+
+singleColor();
+
+function singleColor() {
+  grid.forEach((item) => {
+    item.addEventListener("mouseenter", function (e) {
+      changeSingleColor(item);
+    });
+  });
+}
+
+function changeSingleColor(item) {
+  item.style.backgroundColor = "#81a1c1";
 }
