5de9735d5e3564c4892228bb849cb9120edde172
Mathieu Sviridov <74146348+M-Sviridov@users.noreply.github.com>
2023-02-12 18:58:38 +0700

    Add comments to code and refactor

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const container = document.querySelector(".container");
+const sizeButton = document.querySelector(".size-button");
+const randomButton = document.querySelector(".random-button");
+const plainButton = document.querySelector(".plain-button");
+const shadingButton = document.querySelector(".shading-button");
 
-createGrid(16);
-
+// create a new grid with plain color filling option by default
 function createGrid(size) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
@@ -16,24 +19,28 @@ function createGrid(size) {
   singleColor(grid);
 }
 
+// initialization of the first 16x16 default grid
+createGrid(16);
+
+// change grid item to a single color with opacity 1
 function singleColor(grid) {
   grid.forEach((item) => {
     item.addEventListener("mouseenter", function (e) {
       changeSingleColor(item);
-      item.style.opacity = 1;
     });
   });
 }
 
+// change grid item to a "random" color with opacity 1
 function randomColor(grid) {
   grid.forEach((item) => {
     item.addEventListener("mouseenter", function (e) {
       changeRandomColor(item);
-      item.style.opacity = 1;
     });
   });
 }
 
+// change grid item to an incremental level of opacity until it reaches 1
 function shadingColor(grid) {
   grid.forEach((item) => {
     let opacity = 0;
@@ -45,25 +52,53 @@ function shadingColor(grid) {
   });
 }
 
+// helper function for the singleColor() function
 function changeSingleColor(item) {
   item.style.backgroundColor = "#81a1c1";
+  item.style.opacity = 1;
 }
 
+// helper function for the randomColor() function
 function changeRandomColor(item) {
   const colors = ["#bf616a", "#d08770", "#ebcb8b", "#a3be8c", "#b48ead"];
   const randomIndex = Math.floor(Math.random() * colors.length);
   item.style.backgroundColor = colors[randomIndex];
+  item.style.opacity = 1;
 }
 
+// helper function for the shadingColor() function
 function changeShadingColor(item, opacity) {
   item.style.opacity = opacity;
   item.style.backgroundColor = "#8fbcbb";
 }
 
-const sizeButton = document.querySelector(".size-button");
-const randomButton = document.querySelector(".random-button");
-const plainButton = document.querySelector(".plain-button");
-const shadingButton = document.querySelector(".shading-button");
+// gets the desired new grid size and makes sure it is correct
+function getSize() {
+  let size = prompt("Enter a new grid size (between 1 and 100): ");
+
+  if (checkSize(size) == 0) {
+    console.log("size incorrect");
+    size = getSize();
+  } else {
+    return size;
+  }
+}
+
+// removes the current grid to make place for the new grid
+function removeGrid() {
+  const currentGrid = document.querySelectorAll(".grid");
+
+  currentGrid.forEach((item) => {
+    container.removeChild(item);
+  });
+}
+
+// checks that the size is correct by being between 1 and 100
+function checkSize(size) {
+  if (size < 1 || size > 100) {
+    return 0;
+  }
+}
 
 sizeButton.addEventListener("click", function (e) {
   removeGrid();
@@ -84,28 +119,3 @@ shadingButton.addEventListener("click", function (e) {
   const grid = document.querySelectorAll(".grid");
   shadingColor(grid);
 });
-
-function getSize() {
-  let size = prompt("Enter a new grid size (between 1 and 100): ");
-
-  if (checkSize(size) == 0) {
-    console.log("size incorrect");
-    size = getSize();
-  } else {
-    return size;
-  }
-}
-
-function removeGrid() {
-  const currentGrid = document.querySelectorAll(".grid");
-
-  currentGrid.forEach((item) => {
-    container.removeChild(item);
-  });
-}
-
-function checkSize(size) {
-  if (size < 1 || size > 100) {
-    return 0;
-  }
-}
